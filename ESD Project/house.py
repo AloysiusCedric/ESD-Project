de9cdad9174a58ff70492ce477826224b3d311a5
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-03-30 13:12:45 +0800

    Add files via upload
    a reupload as i mistakenly put it in the wrong directory

--- ESD Project/house.py
+++ ESD Project/house.py
@@ -33,9 +33,27 @@ class House(db.Model):
     def json(self):
         return {"houseID": self.houseID, "houseName": self.houseName, "address": self.address, "region": self.region, "latitude": self.latitude, "longitude": self.longitude, "price": self.price}
 
+@app.route("/house")
+def get_all_houses():
+    houselist = House.query.all()
+    if len(houselist):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "House list": [house.json() for house in houselist]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no available houses."
+        }
+    ), 404
 
-@app.route("/confirmation" , methods=['GET'])
-def get_all():
+@app.route("/confirmation" , methods=['POST'])
+def get_houseId():
     houselist = House.query.all()
     if request.is_json:
         order = request.get_json()
@@ -43,20 +61,20 @@ def get_all():
         return jsonify(result), result["code"]
     else:
         data = request.get_data()
-        print("Received an invalid order:")
+        print("Received an invalid request:")
         print(data)
         return jsonify({"code": 400,
                         # make the data string as we dunno what could be the actual format
                         "data": str(data),
-                        "message": "Order should be in JSON."}), 400  # Bad Request input
-
+                        "message": "Request should be in JSON."}), 400  # Bad Request input
+    
 def processOrder(order):
-    print("Processing an order for shipping:")
+    print("Debug line for helping:")
     print(order)
     # Can do anything here, but aiming to keep it simple (atomic)
-    house_list = order['houseId']
+    house_list = order['houseID']
     # If customer id contains "ERROR", simulate failure
-    if "ERROR" in order['houseId']:
+    if "ERROR" in order['houseID']:
         code = 400
         message = 'Simulated failure in retriving house data.'
     else:  # simulate success
@@ -74,24 +92,6 @@ def processOrder(order):
     }
 
 
-@app.route("/house")
-def get_all():
-    houselist = House.query.all()
-    if len(houselist):
-        return jsonify(
-            {
-                "code": 200,
-                "data": {
-                    "House list": [house.json() for house in houselist]
-                }
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "message": "There are no available houses."
-        }
-    ), 404
 
 
 #@app.route("/house/string:<")
