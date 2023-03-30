770b45c4556cb06eb0ae422281e189c6b9543408
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-03-30 12:48:43 +0800

    Add files via upload
    added in 
    
    @app.route("/confirmation" , methods=['GET'])
    def get_all():
        houselist = House.query.all()
        if request.is_json:
            order = request.get_json()
            result = processOrder(order)
            return jsonify(result), result["code"]
        else:
            data = request.get_data()
            print("Received an invalid order:")
            print(data)
            return jsonify({"code": 400,
                            # make the data string as we dunno what could be the actual format
                            "data": str(data),
                            "message": "Order should be in JSON."}), 400  # Bad Request input
    
    def processOrder(order):
        print("Processing an order for shipping:")
        print(order)
        # Can do anything here, but aiming to keep it simple (atomic)
        house_list = order['houseId']
        # If customer id contains "ERROR", simulate failure
        if "ERROR" in order['houseId']:
            code = 400
            message = 'Simulated failure in retriving house data.'
        else:  # simulate success
            code = 201
            message = 'Simulated success in retriving house data.'
        print(message)
        print()  # print a new line feed as a separator
    
        return {
            'code': code,
            'data': {
                'house_list': house_list
            },
            'message': message
        }
    
    for aloysius to try invoke, not sure if its going to work

--- ESD Project/house.py
+++ ESD Project/house.py
@@ -34,28 +34,67 @@ class House(db.Model):
         return {"houseID": self.houseID, "houseName": self.houseName, "address": self.address, "region": self.region, "latitude": self.latitude, "longitude": self.longitude, "price": self.price}
 
 
-@app.route("/")
+@app.route("/confirmation" , methods=['GET'])
 def get_all():
-    booklist = House.query.all()
-    if len(booklist):
+    houselist = House.query.all()
+    if request.is_json:
+        order = request.get_json()
+        result = processOrder(order)
+        return jsonify(result), result["code"]
+    else:
+        data = request.get_data()
+        print("Received an invalid order:")
+        print(data)
+        return jsonify({"code": 400,
+                        # make the data string as we dunno what could be the actual format
+                        "data": str(data),
+                        "message": "Order should be in JSON."}), 400  # Bad Request input
+
+def processOrder(order):
+    print("Processing an order for shipping:")
+    print(order)
+    # Can do anything here, but aiming to keep it simple (atomic)
+    house_list = order['houseId']
+    # If customer id contains "ERROR", simulate failure
+    if "ERROR" in order['houseId']:
+        code = 400
+        message = 'Simulated failure in retriving house data.'
+    else:  # simulate success
+        code = 201
+        message = 'Simulated success in retriving house data.'
+    print(message)
+    print()  # print a new line feed as a separator
+
+    return {
+        'code': code,
+        'data': {
+            'house_list': house_list
+        },
+        'message': message
+    }
+
+
+@app.route("/house")
+def get_all():
+    houselist = House.query.all()
+    if len(houselist):
         return jsonify(
             {
                 "code": 200,
                 "data": {
-                    "books": [book.json() for book in booklist]
+                    "House list": [house.json() for house in houselist]
                 }
             }
         )
     return jsonify(
         {
             "code": 404,
-            "message": "There are no books."
+            "message": "There are no available houses."
         }
     ), 404
 
 
-
-# @app.route("/book/<string:isbn13>")
+#@app.route("/house/string:<")
 # def find_by_isbn13(isbn13):
 #     book = Book.query.filter_by(isbn13=isbn13).first()
 #     if book:
