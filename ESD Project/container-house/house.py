fb87d64c7eb9277ba49d2540bbca9f48edcb9557
MervinTanZhiYong <115868196+MervinTanZhiYong@users.noreply.github.com>
2023-03-30 21:46:17 +0800

    Add files via upload
    please re-download this house.py
    
    Thanks

--- ESD Project/container-house/house.py
+++ ESD Project/container-house/house.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/g1t3-aangstay'
@@ -11,17 +12,17 @@ db = SQLAlchemy(app)
 class House(db.Model):
     __tablename__ = 'house'
     
-    houseID = db.Column(db.Integer, primary_key=True)
+    houseId = db.Column(db.Integer, primary_key=True)
     houseName = db.Column(db.String(64), nullable=False)
     address = db.Column(db.String(255), nullable=False)
     region = db.Column(db.String(50), nullable=False)
     latitude = db.Column(db.Float(11,8), nullable=False)
-    longitude = db.Column(db.Float(12,8), nullable=False)
+    longitude = db.Column(db.Float(12,8), nullable=False)   
     price = db.Column(db.Float(10,0), nullable=False)
 
 
-    def __init__(self, houseID, houseName, address, region, latitude, longitude, price):
-        self.houseID = houseID
+    def __init__(self, houseId, houseName, address, region, latitude, longitude, price):
+        self.houseId = houseId
         self.houseName = houseName
         self.address = address
         self.region = region
@@ -31,9 +32,74 @@ class House(db.Model):
 
 
     def json(self):
-        return {"houseID": self.houseID, "houseName": self.houseName, "address": self.address, "region": self.region, "latitude": self.latitude, "longitude": self.longitude, "price": self.price}
+        return {"houseId": self.houseId, "houseName": self.houseName, "address": self.address, "region": self.region, "latitude": self.latitude, "longitude": self.longitude, "price": self.price}
 
-@app.route("/house")
+@app.route("/house_record", methods=['POST'])
+def get_houseId():
+    if request.is_json:
+        houseIds = request.json['houseId'].split(',')
+        houses_data = []
+        for houseId in houseIds:
+            # Check if the houseId is valid
+            if not houseId:
+                return {
+                    'code': 400,
+                    'data': None,
+                    'message': 'Invalid houseId'
+                }
+            # Retrieve the housing data based on houseId
+            house_data = requests.get(f'http://127.0.0.1:5003/house/{houseId}').json()
+            if house_data['code'] == 200:
+                houses_data.append(house_data['data'])
+            else:
+                return {
+                    'code': 404,
+                    'data': None,
+                    'message': f'House {houseId} not found'
+                }
+        return {
+            'code': 200,
+            'data': houses_data,
+            'message': 'Housing data retrieved successfully'
+        }
+    else:
+        data = request.get_data()
+        print("Received an invalid request:")
+        return jsonify({
+            "code": 400,
+            "data": str(data),
+            "message": "Request should be in JSON."
+        }), 400
+
+
+def processOrder(order):
+    houseId = int(order['houseId']) # "3" turn to 3
+    
+    # Check if the houseId is valid
+    if not houseId: # "houseId" : "value"
+        return {
+            'code': 400,
+            'data': None,
+            'message': 'Invalid houseId'
+        }
+    
+    # Retrieve the housing data based on houseId
+    house_data = requests.get(f'http://127.0.0.1:5003/house/{houseId}').json() #return
+    
+    if house_data['code'] == 200:
+        return {
+            'code': 200,
+            'data': house_data['data'],
+            'message': 'Housing data retrieved successfully'
+        }
+    else:
+        return {
+            'code': 404,
+            'data': None,
+            'message': 'House not found'
+        }
+        
+@app.route("/house") # normal access to all house data
 def get_all_houses():
     houselist = House.query.all()
     if len(houselist):
@@ -41,7 +107,7 @@ def get_all_houses():
             {
                 "code": 200,
                 "data": {
-                    "House list": [house.json() for house in houselist]
+                    "house-list": [house.json() for house in houselist]
                 }
             }
         )
@@ -51,106 +117,23 @@ def get_all_houses():
             "message": "There are no available houses."
         }
     ), 404
-
-@app.route("/confirmation" , methods=['GET'])
-def get_houseId():
-    houselist = House.query.all()
-    if request.is_json:
-        order = request.get_json()
-        result = processOrder(order)
-        return jsonify(result), result["code"]
-    else:
-        data = request.get_data()
-        print("Received an invalid order:")
-        print(data)
-        return jsonify({"code": 400,
-                        # make the data string as we dunno what could be the actual format
-                        "data": str(data),
-                        "message": "Order should be in JSON."}), 400  # Bad Request input
-    
-def processOrder(order):
-    print("Processing an order for shipping:")
-    print(order)
-    # Can do anything here, but aiming to keep it simple (atomic)
-    house_list = order['houseId']
-    # If customer id contains "ERROR", simulate failure
-    if "ERROR" in order['houseId']:
-        code = 400
-        message = 'Simulated failure in retriving house data.'
-    else:  # simulate success
-        code = 201
-        message = 'Simulated success in retriving house data.'
-    print(message)
-    print()  # print a new line feed as a separator
-
-    return {
-        'code': code,
-        'data': {
-            'house_list': house_list
-        },
-        'message': message
-    }
-
-
-
-
-#@app.route("/house/string:<")
-# def find_by_isbn13(isbn13):
-#     book = Book.query.filter_by(isbn13=isbn13).first()
-#     if book:
-#         return jsonify(
-#             {
-#                 "code": 200,
-#                 "data": book.json()
-#             }
-#         )
-#     return jsonify(
-#         {
-#             "code": 404,
-#             "message": "Book not found."
-#         }
-#     ), 404
-
-
-# @app.route("/book/<string:isbn13>", methods=['POST'])
-# def create_book(isbn13):
-#     if (Book.query.filter_by(isbn13=isbn13).first()):
-#         return jsonify(
-#             {
-#                 "code": 400,
-#                 "data": {
-#                     "isbn13": isbn13
-#                 },
-#                 "message": "Book already exists."
-#             }
-#         ), 400
-
-
-#     data = request.get_json()
-#     book = Book(isbn13, **data)
-
-
-#     try:
-#         db.session.add(book)
-#         db.session.commit()
-#     except:
-#         return jsonify(
-#             {
-#                 "code": 500,
-#                 "data": {
-#                     "isbn13": isbn13
-#                 },
-#                 "message": "An error occurred creating the book."
-#             }
-#         ), 500
-
-
-#     return jsonify(
-#         {
-#             "code": 201,
-#             "data": book.json()
-#         }
-#     ), 201
+        
+@app.route("/house/<string:houseId>")   # retreiving house data based on houseId 
+def find_by_houseId(houseId):
+        house = (House.query.filter_by(houseId=houseId).first())
+        if house:
+            return jsonify(
+                {
+                "code": 200,
+                "data" : house.json()
+                }
+            )
+        return jsonify(
+        {
+            "code": 404,
+            "message": "House name not found."
+        }
+    ), 404
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5003, debug=True)
