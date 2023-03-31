57d2d9528f5e4a33a3416aaa21f83bf6ed0bbf91
ly510 <95639323+ly510@users.noreply.github.com>
2023-03-31 16:50:25 +0800

    -

---
+++ ESD Project/sql/transaction-add.sql
@@ -0,0 +1,7 @@
+INSERT INTO `g1t3-aangstay`.`transaction` (`transactionId`,`startDate`, `endDate`,`status`,`houseId`,`bookingNum`,`paymentId`) VALUES
+('10000001','2023-04-05', '2023-04-09','confirmed','5','3FB3B3','22828919WS328170T'); 
+
+-- Dumping data for transaction db
+INSERT INTO `g1t3-aangstay`.`transaction` (`startDate`, `endDate`,`status`,`houseId`,`bookingNum`,`paymentId`) VALUES
+('2023-04-05', '2023-04-09','confirmed','6', '347F50', '2GC1726685321561L'),
+('2023-04-05', '2023-04-09','cancelled','5', '1CB14F','4AY363202E056942B');
