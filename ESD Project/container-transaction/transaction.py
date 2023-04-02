b9940b3cd014b7249c8c2e7b00abf98112f33100
wja999 <115869409+wja999@users.noreply.github.com>
2023-04-02 17:15:02 +0800

    Add files via upload

--- ESD Project/container-transaction/transaction.py
+++ ESD Project/container-transaction/transaction.py
@@ -13,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Transaction(db.Model):
     transactionId = db.Column(db.String, primary_key=True)
     startDate = db.Column(db.Date, nullable=False)
@@ -35,10 +36,11 @@ def get_current_month_transactions():
     else:
         today = datetime.date.today()
         start_date = datetime.date(today.year, today.month, 1)
-        end_date = datetime.date(today.year, today.month, calendar.monthrange(today.year, today.month)[1])
-        
+        end_date = datetime.date(
+            today.year, today.month, calendar.monthrange(today.year, today.month)[1])
+
     available_house_ids = []
-    
+
     transactions = Transaction.query.filter(
         Transaction.status == "confirmed",
         ((Transaction.startDate <= start_date) & (Transaction.endDate >= end_date)) |
@@ -54,64 +56,74 @@ def get_current_month_transactions():
                     break
         if not overlapping:
             available_house_ids.append(house_id)
-    if len(available_house_ids) != 0 :
-        return jsonify({"code":200,
-                    "data": available_house_ids,
-                    "message": "Available Houses houseids successfully returned."})
-    else :
-        return jsonify({"code":200,
-                    "message": "There are no available houses houseids"})
+    if len(available_house_ids) != 0:
+        return jsonify({"code": 200,
+                        "data": available_house_ids,
+                        "message": "Available Houses houseids successfully returned."})
+    else:
+        return jsonify({"code": 200,
+                        "message": "There are no available houses houseids"})
 
 
-
-@app.route('/transaction', methods=['GET'])
+@app.route('/transaction', methods=['POST'])
 def add_transaction():
     data = request.get_json()
-    transactionId = data['transactionId']
-    houseId = data['houseId']
-    startDate = datetime.datetime.fromisoformat(data['startDate'])
-    endDate = datetime.datetime.fromisoformat(data['endDate'])
-    status = data['status']
-    bookingNum = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    paymentId = data['paymentId']
+    data = data['data']  # extract data object from input
+    houseId = int(data['houseID'])
+    startDate = datetime.datetime.fromisoformat(data['startDate']).date()
+    endDate = datetime.datetime.fromisoformat(data['endDate']).date()
+    paymentId = f"{data['paymentId']}"
+    hPrice = data["hPrice"]
 
-    if status == "confirmed":
-        new_transaction = Transaction(
-            transactionId=transactionId,
-            houseId=houseId,
-            startDate=startDate,
-            endDate=endDate,
-            status=status,
-            bookingNum=bookingNum,
-            paymentId = paymentId
-        )
-        db.session.add(new_transaction)
-        db.session.commit()
+    # Generate transactionId and bookingNum
+    transactionId = ''.join(random.choices(
+        string.ascii_uppercase + string.digits, k=10))
+    bookingNum = ''.join(random.choices(
+        string.ascii_uppercase + string.digits, k=6))
 
-        result = {"code":200,'Message': 'New entry created successfully!', 'bookingNum':bookingNum}
-        return jsonify(result)
-    else:
-        result = {"code":400,'Message': 'Transaction status must be confirmed'}
-        return jsonify(result)
+    # Create new transaction object and add to database
+    new_transaction = Transaction(
+        transactionId=transactionId,
+        houseId=houseId,
+        startDate=startDate,
+        endDate=endDate,
+        status="confirmed",
+        bookingNum=bookingNum,
+        paymentId=paymentId
+    )
+    db.session.add(new_transaction)
+    db.session.commit()
+
+    result = {
+        "code": 201,
+        "message": "Your booking is successful",
+        "data": {
+            "startDate": startDate,
+            "endDate": endDate,
+            "houseId": houseId,
+            "paymentId": paymentId
+        }
+    }
+
+    return jsonify(result)
 
 
 @app.route('/transaction/<string:bookingNum>/cancel', methods=['POST'])
 def cancel_transaction(bookingNum):
     transaction = Transaction.query.filter_by(bookingNum=bookingNum).first()
-    
+
     if transaction:
         transaction.status = 'cancelled'
         db.session.commit()
-        result = {"code":200,
-                    'message': f'Transaction with bookingNum {bookingNum} has been cancelled.',
-                   'paymentId': transaction.paymentId}
+        result = {"code": 200,
+                  'message': f'Transaction with bookingNum {bookingNum} has been cancelled.',
+                  'paymentId': transaction.paymentId}
         return jsonify(result)
-    else:   
-        result = {"code":404,'message': f'Transaction with bookingNum {bookingNum} not found in the database.'}
+    else:
+        result = {
+            "code": 404, 'message': f'Transaction with bookingNum {bookingNum} not found in the database.'}
         return jsonify(result)
-
-
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
